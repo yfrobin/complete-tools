045994f466879e8f9405560bcedb6c38abacc025
yefei <wb-yefei@tongdun.net>
2019-07-05 11:46:52 +0800

    complete-tools第一次提交

---
+++ src/main/java/com/yf/completetools/CompleteToolsApplication.java
@@ -0,0 +1,13 @@
+package com.yf.completetools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompleteToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompleteToolsApplication.class, args);
+    }
+
+}
